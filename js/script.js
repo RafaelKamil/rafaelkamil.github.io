7cd79c0a1cfe1744c04a96e4f4eff552e20e0ff0
Lee Quan Sheng <52743433+iamgruuten@users.noreply.github.com>
2024-01-16 05:33:19 +0800

    added projects

--- js/script.js
+++ js/script.js
@@ -8,12 +8,33 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
 
 let mediumArticles = [];
 let scalerArticles = [{
-    'link': './report1.pdf',
+    'link': 'https://medium.com/@s10192797/digital-forensics-research-3e08efbe384a',
     'title': 'Forensic and security research',
     'pubDate': 'December 24, 2021',
     'content': 'This assignment explores tools for efficient forensic analysis. Dcfldd ensures disk integrity, extracting key information. Bulk-extractor uncovers hidden data like email addresses and credit card numbers. The aim is to gather evidence systematically.',
-    'thumbnail': './images/ff.png',
-}];
+    'thumbnail': './report1.pdf',
+},
+{
+    'link': 'https://medium.com/@s10192797/cloud-architecture-and-technologies-setup-927aa8bb0504',
+    'title': 'Cloud Computing Architecture Solution',
+    'pubDate': 'December 24, 2021',
+    'content': 'A demostration of implementing cloud architecture. Designed and built Cloud Architecture which offers scalability ensuring that website effortlessly scales to meet growing demands, accommodating increased traffic and evolving user needs without the limitations of traditional infrastructure.',
+    'thumbnail': 'https://medium.com/@s10192797/cloud-architecture-and-technologies-setup-927aa8bb0504',
+},{
+    'link': 'https://github.com/iMaybeCanBreathe/Helpderly',
+    'title': 'Helpderly Project',
+    'pubDate': 'December 24, 2021',
+    'content': 'Our platform connects volunteers with impactful events while providing an easy way for the elderly to seek assistance. Simplifying registration for volunteers and requests for support, we aim to build a community where compassion knows no boundaries.',
+    'thumbnail': 'https://github.com/iMaybeCanBreathe/Helpderly',
+},{
+    'link': 'https://medium.com/@s10192797/smart-rack-project-36a0c63ab3e0',
+    'title': 'Smart Clothes Rack',
+    'pubDate': 'December 24, 2021',
+    'content': 'The Smart Clothes Rack with Rain Detection project combines Internet of Things (IoT) technologies to create an innovative solution for protecting clothes from rain and moisture. The system utilizes a Raspberry Pi as the central processing unit, a water sensor for rain detection, and a retract mechanism for automatically adjusting the clothes rack.',
+    'thumbnail': 'https://medium.com/@s10192797/smart-rack-project-36a0c63ab3e0',
+}]
+
+;
 
 scalerArticles.sort(function(a, b) {
     return new Date(b.date) - new Date(a.date);
@@ -28,17 +49,19 @@ const getArticleDOM = function(article) {
 
     const pubDate = `${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 
-    const imgTag = article.thumbnail.length > 0 ? `<img src="${article.thumbnail}" alt="${article.title}">` : "";
+    const imgTag = `${article.thumbnail}`
 
-    return `<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 blog box no-border no-padding">
+    return `<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 blog box no-border no-padding">
                 <a href="${articleLink}" target="_blank">
-                    ${imgTag}
                     <div class="content">
-                        <h5>${article.title}</h5>
-                        <p class="description">${content}</p>
+                        <h4>${article.title}</h4>
+                        <p class="description">${content}  <a style="font-size:0.6rem; color:#0d6efd!important;" href="${articleLink}">read more...</a></p>
+                        <a href="${imgTag}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+
                     </div>
                 </a>
-            </div>`;
+            </div>
+            `;
 }
 
 const getArticles = function() {
